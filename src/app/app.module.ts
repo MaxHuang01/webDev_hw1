7f90a7c98b45e40c7c504279b747285a2b2eb28c
maxhuang <a5539685@alum.ccu.edu.tw>
2020-11-02 23:52:03 -0600

    hw3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TravleRequestComponent } from './travle-request/travle-request.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: 'about', component:AboutComponent},
+  {path: 'flightPrice', component:TravleRequestComponent},
+  {path: '', redirectTo: '/about', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    TravleRequestComponent
-  ],
+    TravleRequestComponent,
+      AboutComponent
+   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
